e217512cf28e0c45eb7c780ca48de809a63d42df
Jimmy-Ma <majing717@163.com>
2016-09-26 18:41:02 +0800

    004.自动装配

---
+++ src/com/jimmy/spring/beans/autowire/Main.java
@@ -0,0 +1,16 @@
+package com.jimmy.spring.beans.autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-autowire.xml");
+		
+		Person person = (Person) ctx.getBean("person");
+		System.out.println(person);
+	}
+
+}
