c32ba7ca10afc06c4fa0355cd7561270b557663d
Jimmy-Ma <majing717@163.com>
2016-09-29 15:16:48 +0800

    014.泛型依赖注入

---
+++ src/com/jimmy/spring/beans/generic/di/Main.java
@@ -0,0 +1,16 @@
+package com.jimmy.spring.beans.generic.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-generic-di.xml");
+		
+		UserService userService = (UserService) ctx.getBean("userService");
+		
+		userService.add();
+	}
+
+}
